e857a4121ad057352bb2b0e4e1593a6e436f533d
hadrienjeanne <hadrien.jeanne@free.fr>
2023-06-05 11:58:50 -0400

    Integrated react in the pipeline
    npm run dev to bundle new version of react page with webpack

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = 'Hello webpack';
-    return element;
-  }
+import React from 'react';
+import ReactDOM from "react-dom";
 
-document.body.appendChild(component());
+ReactDOM.render(
+  <h1>Hello, react!</h1>,
+  document.getElementById('root')
+);
