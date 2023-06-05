692f3a044cfca199763fd90cb5da9dd9296c18b8
hadrienjeanne <hadrien.jeanne@free.fr>
2023-06-05 12:37:47 -0400

    Created the name model

--- insee_names/models.py
+++ insee_names/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Name(models.Model):
+    SEX_CHOICES = (
+        ('1', 'Masculin'),
+        ('2', 'Féminin'),
+    )
+    sex = models.CharField(max_length=8, choices=SEX_CHOICES)
+    firstname = models.CharField(max_length=25)
+    birthyear = models.CharField(max_length=4)
+    quantity = models.IntegerField(max_length=8)
